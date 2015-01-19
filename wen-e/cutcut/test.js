f90db46b4b67118bcdbd08462132495d30baa00c
type0 <ghyghygg30@gmail.com>
2015-01-20 03:35:56 +0800

    term module improve

--- wen-e/cutcut/test.js
+++ wen-e/cutcut/test.js
@@ -7,9 +7,23 @@ var sentence = '寒假基本上練球還是禮拜四跟禮拜天';
 
 async.series([
 	function(callback){
+		terms.gen_by_word("我",callback);
+	},
+	function(callback){
+		terms.gen_by_word("是",callback);
+	},
+	function(callback){
+		terms.gen_by_word("陳",callback);
+	},
+	function(callback){
 		terms.gen_by_word("秋",callback);
-	}
+	},
+	function(callback){
+		terms.gen_by_word("中",callback);
+	},
 ],function(err,res){
-	console.log(res[0]);
+	if(err)console.log(err)
+	for (var i = 0; i < res.length; i++) {
+		console.log(res[i])
+	};
 });
-
